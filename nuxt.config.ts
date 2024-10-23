5c4c5154f6e661093aa8346b94f969c228d067a5
ngtuonghy <ngtuonghy@gmail.com>
2024-10-23 18:21:05 +0700

    fix google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,11 +18,12 @@ export default defineNuxtConfig({
 				test: false,
 			},
 		],
+
 		"nuxt-gtag",
 	],
 	gtag: {
-		id: process.env.GOOGLE_ANALYTICS_ID,
-		enabled: process.env.NODE_ENV === "production",
+		id: "G-559VEZH40V",
+		enabled: true,
 	},
 
 	components: [
