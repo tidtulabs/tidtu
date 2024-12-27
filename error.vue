30702593a2cdedb123c2cc66b884fe06c1bf3311
ngtuonghy <ngtuonghy@gmail.com>
2024-12-27 08:06:41 +0700

    fetch_more 3 and delete not using

--- error.vue
+++ error.vue
@@ -12,10 +12,10 @@ const props = defineProps({
       <h1
         class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary"
       >
-        {{ error.statusCode }}
+        {{ error?.statusCode }}
       </h1>
       <p class="mb-8 text-xl font-semibold text-gray-600 dark:text-gray-400">
-        {{ error.message || "An error occurred" }}
+        {{ error?.message || "An error occurred" }}
       </p>
       <NuxtLink to="/">
         <Button> Back to home </Button>
