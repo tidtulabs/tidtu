629021d692cb002c8954c4dd7bf14813a59eb746
ngtuonghy <ngtuonghy@gmail.com>
2026-07-16 15:57:41 +0700

    hotfix: icon app

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -20,7 +20,6 @@ useHead({
     { name: "twitter:image", content: "https://tidtu.pages.dev/og-image.png" },
   ],
   link: [
-    { rel: "icon", href: "/favicon.ico", sizes: "any" },
     { rel: "icon", href: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
     { rel: "icon", href: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
     { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
